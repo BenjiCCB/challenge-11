2cca8a1b1a2c83a20834de73b60698831823c027
BenjiCCB <bensdecker+CCB@gmail.com>
2023-01-30 14:26:36 -0500

    progress

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+// MiddleWare
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routing
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
@@ -11,11 +16,11 @@ app.get('/notes', (req, res) =>
 );
 
 app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received`);
     console.info(`${req.method} request received`);
     console.info(req.body);
+    res.json(`${req.method} request received`);
   });
 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
