0dbc8c9a8ab94f082e1865cab10ad8f2b5ab9d2a
BenjiCCB <bensdecker+CCB@gmail.com>
2023-01-30 15:01:49 -0500

    working on file read

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
@@ -15,11 +16,23 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received`);
+  fs.readFile("./db/db.json", 
+    (err, data) => err ? console.error(err) : console.log ("success reading \n" + data)
+  );
+
+  res.json(`${req.method} request received -testing`);
+
+
+});
+
+
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received`);
-    console.info(req.body);
-    res.json(`${req.method} request received`);
-  });
+  console.info(`${req.method} request received`);
+  console.info(req.body);
+  res.json(`${req.method} request received`);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
